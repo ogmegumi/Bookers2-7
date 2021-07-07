924e05ac3b44f61317758e92b09a7860addc31de
ogmegumi <ogmegumi@gmail.com>
2021-07-07 16:26:29 +0000

    finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -12,7 +12,8 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-   BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
    #redirect_to book_path(params[:book_id])
   end
 
