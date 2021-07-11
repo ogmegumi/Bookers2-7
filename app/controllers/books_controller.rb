605ae4354afc2ffd1a50bc4e4524d4cb02daa99c
ogmegumi <ogmegumi@gmail.com>
2021-07-11 00:33:03 +0000

    [update]order.sort

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,58 +1,61 @@
 class BooksController < ApplicationController
-before_action :correct_user, only: [:edit]
+   before_action :authenticate_user!
+   before_action :correct_user, only: [:edit, :update, :destroy]
+   helper_method :sort_column, :sort_direction
 
-  def show
-    @book = Book.find(params[:id])
-    @book_new = Book.new
-    @user = @book.user
-    @book_comment = BookComment.new
-    @book_comments = @book.book_comments
-  end
-
-  def index
-    @books = Book.all
-    @book = Book.new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
-    else
-      @books = Book.all
-      render 'index'
+    def show
+     @book = Book.find(params[:id])
+     @book_new = Book.new
+     @user = @book.user
+     @book_comment = BookComment.new
+     @book_comments = @book.book_comments
     end
-  end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
+   def index
+        @books = Book.all.order(params[:sort])
+        @book = Book.new
+   end
 
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
-    else
-      render "edit"
+   def create
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     if @book.save
+       redirect_to book_path(@book), notice: "You have created book successfully."
+     else
+       @books = Book.all
+       render 'index'
+     end
+   end
+
+   def edit
+     @book = Book.find(params[:id])
+   end
+
+   def update
+     @book = Book.find(params[:id])
+     if @book.update(book_params)
+       redirect_to book_path(@book), notice: "You have updated book successfully."
+     else
+       render "edit"
+     end
+   end
+
+   def destroy
+     @book = Book.find(params[:id])
+     @book.destroy
+     redirect_to books_path
+   end
+
+   private
+
+    def book_params
+     params.require(:book).permit(:title, :body, :rate)
     end
-  end
 
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
-  end
-
-  private
-
-  def book_params
-    params.require(:book).permit(:title, :body, :rate)
-  end
-
-  def correct_user
-    @book = Book.find(params[:id])
-    redirect_to(books_url) unless @book.user == current_user
-  end
-
+   def correct_user
+     @book = Book.find(params[:id])
+     redirect_to(books_url) unless @book.user == current_user
+   end
+  
+  
 end
